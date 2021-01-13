5a327039776b5aff141b9b230b19f04413b1cc83
Jacob Sardo <jsardo@nd.edu>
2021-01-12 19:35:42 -0500

    Tests / Organization

---
+++ src/DriverDatabaseTester.java
@@ -0,0 +1,37 @@
+public class DriverDatabaseTester {
+    public static void main(String[] args) {
+        // Creating a sample Database
+        DriverDatabase testDB = new DriverDatabase();
+
+        // Creating sample drivers with sample trips
+        Driver Dave = new Driver("Dave");
+        Dave.addTrip(new Trip(730, 760, 5.4));
+        Dave.addTrip(new Trip(1120, 1155, 9.5));
+        Dave.addTrip(new Trip(1301, 1355, 16.2));
+
+        Driver Alex = new Driver("Alex");
+        Alex.addTrip(new Trip(11, 95, 30.3));
+        Alex.addTrip(new Trip(1200, 1342, 70.5));
+
+        Driver James = new Driver("James");
+        James.addTrip(new Trip(130, 350, 56.6));
+        James.addTrip(new Trip(444, 560, 30.5));
+        James.addTrip(new Trip(700, 720, 15.2));
+
+        // Running Basic Tests
+        System.out.println("Testing addDriver() Function");
+        testDB.addDriver(Dave);
+        testDB.addDriver(Alex);
+        testDB.addDriver(James);
+        System.out.println(testDB);
+        System.out.println("Testing getDriver() Function");
+        System.out.println("Retreiving Driver 'James':");
+        System.out.println(testDB.getDriver("James") + "\n");
+        System.out.println("Testing removeDriver() Function");
+        System.out.println("Removing Driver 'Alex':");
+        testDB.removeDriver("Alex");
+        System.out.println(testDB);
+        System.out.println("Testing printByMileage() Function");
+        testDB.printByMileage();
+    }
+}
--- src/TripTester.java
+++ src/TripTester.java
@@ -1,6 +1,20 @@
 public class TripTester {
     public static void main(String[] args) {
+        // Creating a sample Trip
         Trip testTrip = new Trip(130, 350, 55.3);
-        System.out.println(testTrip);
+
+        // Running Basic Tests
+        System.out.println("Testing Overridden toString() Function");
+        System.out.println(testTrip + "\n");
+        System.out.println("Testing getDuration() Function");
+        System.out.println("Trip Duration: " + testTrip.getDuration() + "\n");
+        System.out.println("Testing getEndTime() Function");
+        System.out.println("Trip End Time: " + testTrip.getEndTime() + "\n");
+        System.out.println("Testing getMiles() Function");
+        System.out.println("Trip Miles: " + testTrip.getMiles() + "\n");
+        System.out.println("Testing getMPH() Function");
+        System.out.println("Trip MPH: " + testTrip.getMPH() + "\n");
+        System.out.println("Testing getStartTime() Function");
+        System.out.println("Trip Start Time: " + testTrip.getStartTime());
     }
 }
