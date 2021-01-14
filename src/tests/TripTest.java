353eee3923248b7ca6276ac2e1606dc1b2eda057
Jacob Sardo <jsardo@nd.edu>
2021-01-13 19:24:29 -0500

    Improved Testability

---
+++ src/tests/TripTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class TripTest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testPrintMessage() {
+        assertEquals(0, 0);
+    }
+
+}
